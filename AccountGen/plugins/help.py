c27ad59a4432a3135a162a43611b8ad66585c576
wearedevs7 <80753332+wearedevs7@users.noreply.github.com>
2021-03-21 10:02:33 +0530

    Update help.py

--- AccountGen/plugins/help.py
+++ AccountGen/plugins/help.py
@@ -16,7 +16,6 @@ async def helper(event):
     text="I am an Account Generator Bot!\nI can generate working accounts for you.\n\nClick generate accounts to get your account!! Make sure to join my channel and support me!"
     await event.edit(text,
                      buttons=[
-                         [Button.url("Channel", url=ltc), Button.url("Dev", url="https://t.me/BotzHub")],
-                         [Button.url("Repository", url="https://github.com/xditya/AccountsGenBot")],
+                         [Button.url("Channel", url=ltc), 
                          [Button.inline("Generate Accounts", data="gen")]
                      ])
